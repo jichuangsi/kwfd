4bc19169b7ec2b9819fcf85316716090fad6784a
jichuangsi <school@jichuangsi>
2019-06-20 19:05:04 +0800

    [jiewen.wu]update db and data

--- Conf/user.php
+++ Conf/user.php
@@ -15,5 +15,5 @@
 define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', '>|Zvb$wE^Jhmp;kd_goi4)PF75@yqK<3QeU6~=sT'); //加密KEY
-define('UC_DB_DSN', 'mysqli://root:root@127.0.0.1:3306/thinkPHP'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysqli://root:root@127.0.0.1:3306/jcszaixian'); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 'onethink_'); // 数据表前缀，使用Model方式调用API必须配置此项
